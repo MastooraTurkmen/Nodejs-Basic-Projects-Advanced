6806dec1c9496cab1a0b3978ee8a166826f9639c
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-04-23 21:06:17 +0430

    patch and delete request

--- natours/app.js
+++ natours/app.js
@@ -47,6 +47,38 @@ app.post('/api/v1/tours', (req, res) => {
     })
 })
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID"
+        })
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour: "<Updated tour here>"
+        }
+    })
+})
+
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID"
+        })
+    }
+    res.status(200).json({
+        status: "success",
+        data: null
+    })
+})
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
