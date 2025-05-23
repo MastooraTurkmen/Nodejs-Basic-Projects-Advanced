295d70fe7944d790acc1a775f8c56dcdc4007a99
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-05-23 21:01:04 +0430

    adds user

--- natours/example/app.js
+++ natours/example/app.js
@@ -1,7 +1,10 @@
 const fs = require("fs")
 const express = require("express")
+const morgan = require("morgan")
 
 const app = express()
+
+app.use(morgan("dev"))
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -79,6 +82,9 @@ const deleteTour = (req, res) => {
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour)
 app.route('/api/v1/tours/:id').get(getSingleTour).patch(updateTour).delete(deleteTour)
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser)
+app.route('/api/v1/users/:id').get(getSingleUser).patch(updateUser).delete(deleteUser)
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
