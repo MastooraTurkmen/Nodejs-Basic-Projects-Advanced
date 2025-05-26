c4b87173044723aa322f09bd4575a27b00d60f44
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-05-26 21:34:58 +0430

    refactor the code

--- natours/example/app.js
+++ natours/example/app.js
@@ -1,6 +1,7 @@
-const fs = require("fs")
 const express = require("express")
 const morgan = require("morgan")
+const tourRouter = require("./routes/tourRoutes")
+const userRouter = require("./routes/userRoutes")
 
 const app = express()
 
@@ -12,111 +13,8 @@ app.use((req, res, next) => {
     next()
 })
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+// Routes
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
-const getAllTours = (req, res) => {
-    res.status(200).json({
-        status: "success",
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
-}
-
-const createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1
-    const newTour = Object.assign({ id: newId }, req.body)
-
-    tours.push(newTour)
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: "success",
-            data: {
-                tours: newTour
-            }
-        })
-    })
-}
-
-const getSingleTour = (req, res) => {
-    console.log(req.params)
-
-    const id = req.params.id * 1
-    const tour = tours.find(el => el.id === id)
-
-    if (!tour) {
-        return res.status(404).json({
-            status: "fail",
-            message: "Tour not found"
-        })
-    }
-
-    res.status(200).json({ status: "success", data: { tour } })
-}
-
-const updateTour = (req, res) => {
-    const id = req.params.id * 1
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: "fail",
-            message: "Tour not found"
-        })
-    }
-    res.status(200).json({ status: "success", data: { tour: "<Updated tour here...>" } })
-}
-
-const deleteTour = (req, res) => {
-    const id = req.params.id * 1
-    const tour = tours.find(el => el.id !== id)
-
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: "fail",
-            message: "Tour not found"
-        })
-    }
-    res.status(204).json({ status: "success", data: { tour } })
-}
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour)
-app.route('/api/v1/tours/:id').get(getSingleTour).patch(updateTour).delete(deleteTour)
-
-
-const getAllUsers = (req, res) => {
-    res.status(200).json({
-        status: "success",
-    })
-}
-
-const createUser = (req, res) => {
-    res.status(201).json({
-        status: "success",
-    })
-}
-
-const getSingleUser = (req, res) => {
-    res.status(200).json({
-        status: "success",
-    })
-}
-
-const updateUser = (req, res) => {
-    res.status(200).json({
-        status: "success",
-    })
-}
-
-const deleteUser = (req, res) => {
-    res.status(204).json({
-        status: "success",
-    })
-}
-
-app.route('/api/v1/users').get(getAllUsers).post(createUser)
-app.route('/api/v1/users/:id').get(getSingleUser).patch(updateUser).delete(deleteUser)
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+module.exports = app
