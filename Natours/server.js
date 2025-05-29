b7f792b46788063f62c91b8fb6afe24281f9644a
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-05-29 21:25:34 +0430

    clean up

--- Natours/server.js
+++ Natours/server.js
@@ -1,38 +1,9 @@
 const dotenv = require("dotenv")
 dotenv.config()
+const mongoose = require("mongoose")
 const app = require("./app")
-const { default: mongoose } = require("mongoose")
 
 mongoose.connect(process.env.MONGO_DB)
-
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true,
-    },
-    rating: {
-        type: Number,
-        default: 4.5,
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price'],
-    },
-})
-
-const Tour = mongoose.model('Tour', tourSchema)
-
-const testTour = new Tour({
-    name: 'The Park',
-    price: 333
-})
-
-testTour.save().then(() => {
-    console.log('Tour saved successfully');
-}).catch(err => {
-    console.log('Error saving tour:', err);
-})
 
 const port = process.env.PORT || 3000
 
