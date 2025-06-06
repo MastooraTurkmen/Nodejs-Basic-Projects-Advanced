502a034256f07859b0fa9a3221c1fc4ccfcf40dd
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-06-06 21:08:57 +0430

    format userController import for better readability

--- Natours/routes/userRoutes.js
+++ Natours/routes/userRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express")
 
 const router = express.Router()
-const { getAllUsers, createUser, getSingleUser, updateUser, deleteUser } = require("../controllers/userController")
+const {
+    getAllUsers,
+    createUser,
+    getSingleUser,
+    updateUser,
+    deleteUser
+} = require("../controllers/userController")
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getSingleUser).patch(updateUser).delete(deleteUser)
