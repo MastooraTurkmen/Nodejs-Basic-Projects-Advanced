86907fa6da175e16718653fed46008dc32e0dca1
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-06-12 21:00:41 +0430

    document middleware

--- Natours/models/tourModel.js
+++ Natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const slugify = require("slugify")
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -53,7 +54,42 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    },
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+})
+
+tourSchema.virtual("durationWeeks").get(function () {
+    return this.duration / 7
+})
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre("save", function () {
+    this.slug = slugify(this.name, { lower: true })
+    next()
+})
+
+tourSchema.pre('save', function (next) {
+    console.log('Will save document...')
+    next()
+})
+
+tourSchema.post('save', function (doc, next) {
+    console.log(doc)
+    next()
+})
+
+
+// QUERY MIDDLEWARE
+
+tourSchema.pre('find', function (next) {
+    this.find({ secretTour: { $ne: true } })
+    next()
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
