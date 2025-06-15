89227164262741da0911f1eaa6bfdd5931bd1314
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-06-15 21:22:51 +0430

    query and aggregation middleware

--- Natours/models/tourModel.js
+++ Natours/models/tourModel.js
@@ -7,6 +7,8 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true,
         trim: true,
+        maxlength: [40, "A tour name must have less or equal than 40 characters"],
+        minlength: [10, "A tour name must have more  or equal than 10 characters"]
     },
     duration: {
         type: Number,
@@ -19,10 +21,16 @@ const tourSchema = new mongoose.Schema({
     difficulty: {
         type: String,
         required: [true, "A tour must have a difficulty"],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, or difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
         default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
         type: Number,
@@ -87,8 +95,25 @@ tourSchema.virtual("durationWeeks").get(function () {
 
 // QUERY MIDDLEWARE
 
-tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
     this.find({ secretTour: { $ne: true } })
+
+    this.start = Date.now()
+
+    next()
+})
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds!`)
+    console.log(docs)
+    next()
+})
+
+// Aggregation middleware
+
+tourSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } })
+    console.log(this.pipeline())
     next()
 })
 
