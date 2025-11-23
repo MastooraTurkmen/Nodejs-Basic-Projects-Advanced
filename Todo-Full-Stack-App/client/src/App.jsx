f57464edb29488f630976711df7a37ca7921ace1
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-11-23 19:19:36 +0430

    setup todo full stack app

---
+++ Todo-Full-Stack-App/client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import ListHeader from './components/ListHeader'
+
+function App() {
+  return (
+  <div className="app">
+    <ListHeader listName={'Holiday'}>
+
+    </ListHeader>
+  </div>
+  )
+}
+
+export default App
