9af98e5758c81ab746ae51894e6e10f46f8d860a
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-11-24 17:09:19 +0430

    add sign out functionality and update list header layout

--- Todo-Full-Stack-App/client/src/components/ListHeader.jsx
+++ Todo-Full-Stack-App/client/src/components/ListHeader.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const ListHeader = ({listName}) => {
+
+  const signOut=()=>{
+    console.log("Signing out...")
+  }
+
   return (
     <div className='list-header'>
+      <h1>
       {listName}
+      </h1>
+      <div className="button-container">
+        <button className="create">Add new</button>
+        <button className="signout" onClick={signOut}>Sign out</button>
+      </div>
     </div>
   )
 }
