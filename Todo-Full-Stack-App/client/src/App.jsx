9318f083b4c5160d390323e971896a886cb34891
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-11-24 17:09:02 +0430

    fetch todos data and update list header name

--- Todo-Full-Stack-App/client/src/App.jsx
+++ Todo-Full-Stack-App/client/src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ListHeader from './components/ListHeader'
 
 function App() {
+
+  const getData = async ()=>{
+    const userEmail = 'mastooraturkmen@gmail.com'
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+ 
   return (
   <div className="app">
-    <ListHeader listName={'Holiday'}>
+    <ListHeader listName={'Holiday tick list'}>
 
     </ListHeader>
   </div>
