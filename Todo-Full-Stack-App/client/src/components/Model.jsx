fb6063f1ea3ab13f1ea7d4de430889dcb09469a6
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-11-26 15:43:49 +0430

    setup modal

--- Todo-Full-Stack-App/client/src/components/Model.jsx
+++ Todo-Full-Stack-App/client/src/components/Model.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import { useState } from "react"
 
-const Model = () => {
+const Model = ({mode, setShowModel}) => {
+  const editMode = mode === 'edit' ? true : false
+
+  const [data, setData] = useState({
+    user_email: "",
+    title: "",
+    progress: "",
+    date: editMode ? "": new Date()
+  })
+
+  const handleChange = (e)=>{ 
+    e.preventDefault()
+    console.log('changing',e.target.value)
+    const {name, value} = e.target
+    setData((data)=>({
+      ...data,
+      [name]: value
+    }))
+
+    console.log(data)
+  }
+
   return (
-    <div>Model</div>
+    <div className="overlay">
+      <div className="modal">
+        <div className="form-title-container">
+          <h3>Let's {mode} your task</h3>
+          <button onClick={()=> setShowModel(false)}>X</button>
+        </div>
+
+        <form>
+          <input 
+            required 
+            maxLength={30} 
+            placeholder="Your task goes here" 
+            type="text" 
+            name="title"
+            value={data.title}
+          />
+          <br/>
+          <label htmlFor="range">Drag to select your current progress</label>
+          <input type="range" name="progress" value={data.progress} onChange={handleChange} min="0" max="100"/>
+          <input type="submit" className={mode}/>
+        </form>
+      </div>
+    </div>
   )
 }
 
